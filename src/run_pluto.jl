fa0c37b74605515c378f5ce44274b9e4f2fe3ebc
Albert Brouwer <brouwer@iiasa.ac.at>
2022-08-31 14:50:18 +0200

    Cleanup and avoid warning

--- src/run_pluto.jl
+++ src/run_pluto.jl
@@ -1,5 +1,11 @@
-using Pkg
-pkg"activate ."
+import Pkg
+Pkg.activate(".")
+cd("notebooks")
 import Pluto
-cd("/processing/notebooks")
-Pluto.run(host="0.0.0.0", notebook="Plots.jl", launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false)
+Pluto.run(
+    host="0.0.0.0",
+    notebook="Plots.jl",
+    launch_browser=false,
+    require_secret_for_open_links=false,
+    require_secret_for_access=false
+)
