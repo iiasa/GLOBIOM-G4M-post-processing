8564d872590d8de3c53da2f835cd6109b564aa64
Albert Brouwer <brouwer@iiasa.ac.at>
2022-08-31 11:59:32 +0200

    Stop requiring secret for browser auth

--- src/run_pluto.jl
+++ src/run_pluto.jl
@@ -1,3 +1,3 @@
 import Pluto
 cd("/processing/notebooks")
-Pluto.run(host="0.0.0.0", notebook="Plots.jl", launch_browser=false)
+Pluto.run(host="0.0.0.0", notebook="Plots.jl", launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false)
